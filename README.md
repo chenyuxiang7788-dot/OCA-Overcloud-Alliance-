89bc93585bdc0c2627acde363dd8f6b719517967
chenyuxiang7788-dot <chenyuxiang7788@gmail.com>
2026-07-13 19:04:14 +0000

    优化了一些问题

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # OCA-Overcloud-Alliance-
 为云上联盟打造的geofs涂装社区
+only overvloud alliance
--- oca-livery-selector.user.js
+++ oca-livery-selector.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         GeoFS 云上联盟涂装 (OCA Livery Selector)
-// @namespace    https://github.com/chenyuxiang7788-dot/oca-livery-selector
+// @namespace    https://github.com/chenyuxiang7788-dot/OCA-Overcloud-Alliance-
 // @version      1.0.0
 // @description  云上联盟 (Overcloud Alliance) 专属涂装选择器
 // @author       Overcloud Alliance
 // @match        https://geo-fs.com/geofs.php*
 // @match        https://*.geo-fs.com/geofs.php*
-// @icon         https://raw.githubusercontent.com/chenyuxiang7788-dot/oca-livery-selector/main/assets/oca-logo.svg
+// @icon         https://raw.githubusercontent.com/chenyuxiang7788-dot/OCA-Overcloud-Alliance-/main/asstets/oca-logo.svg
 // @grant        none
 // @run-at       document-idle
 // ==/UserScript==
@@ -15,7 +15,7 @@
   'use strict';
 
   const MAIN_JS_URL =
-    'https://raw.githubusercontent.com/chenyuxiang7788-dot/oca-livery-selector/main/main.js';
+    'https://raw.githubusercontent.com/chenyuxiang7788-dot/OCA-Overcloud-Alliance-/main/main.js';
 
   fetch(MAIN_JS_URL)
     .then((res) => {
